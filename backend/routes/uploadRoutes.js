2fc1f0a2c9e6adeb2c099dd7228de189c0172bae
CHETAN AHIRE <chetan.s012006@gmail.com>
2026-08-12 22:44:26 +0530

    Fix HTTPS uploaded image URLs

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -60,7 +60,7 @@ router.post("/", upload.single("image"), (req, res) => {
         }
 
         const imageUrl =
-            `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+    `https://recipe-finder-gdak.onrender.com/uploads/${req.file.filename}`;
 
         res.json({
             imageUrl: imageUrl
